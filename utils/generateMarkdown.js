93ebd2479ae180197ce4b4c5bcc45c6b1425d072
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-07 21:51:15 -0800

    Update index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,21 +3,21 @@
 
 //If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-   
+![GitHub]("https://img.shields.io/github/license/Alma-Dev914/Readme-Generator?style=for-the-badge");
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
-
+![GitHub]("https://img.shields.io/github/license/Alma-Dev914/Readme-Generator")
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
-
-       
+![GitHub]("https://img.shields.io/github/license/Alma-Dev914/Readme-Generator")
               
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+     
 `;
 }
 
